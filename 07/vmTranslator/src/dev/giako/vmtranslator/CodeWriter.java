ef6d61071ebbb71ccbbace11e10f97032fcbfde8
Giacomo Russo <giako@giako.dev>
2020-06-07 00:01:00 +0200

    Stage 1 VM Translator complete
    All mathemantical and logic operations are done and tested, including the push constant N memory operation.

--- 07/vmTranslator/src/dev/giako/vmtranslator/CodeWriter.java
+++ 07/vmTranslator/src/dev/giako/vmtranslator/CodeWriter.java
@@ -7,8 +7,9 @@ import java.io.PrintWriter;
  * Translates VM commands into Hack assembly code.
  */
 public class CodeWriter implements Closeable {
-    private PrintWriter printWriter;
+    private final PrintWriter printWriter;
     private String fileName;
+    private int labelLogicNumber = 0;
 
     /**
      * Opens the input file/stream and gets ready to write into it.
@@ -31,7 +32,130 @@ public class CodeWriter implements Closeable {
      * @param command the arithmetic command to translate
      */
     public void writeArithmetic(String command) {
-        // TODO Implement
+        switch (command) {
+            case "add":
+                addressTopmostStackElementAndStoreInDRegister();
+
+                // Retrieve second topmost element on stack and add it to register, and store result on second topmost
+                // element
+                decrementARegister(1); // @SP-2
+                printWriter.println("M=D+M");
+
+                decreaseSP();
+                break;
+
+            case "sub":
+                addressTopmostStackElementAndStoreInDRegister();
+
+                // Retrieve second topmost element on stack and subtract it from register, and store result on second
+                // topmost element
+                decrementARegister(1); // @SP-2
+                printWriter.println("M=M-D");
+
+                decreaseSP();
+                break;
+
+            case "neg":
+                addressTopmostStackElementAndStoreInDRegister();
+
+                // Store in topmost element negative register, leave SP unaltered
+                printWriter.println("M=-D");
+                break;
+
+            case "eq":
+                generateLogicInstruction("JEQ");
+                break;
+
+            case "gt":
+                generateLogicInstruction("JGT");
+                break;
+
+            case "lt":
+                generateLogicInstruction("JLT");
+                break;
+
+            case "and":
+                addressTopmostStackElementAndStoreInDRegister();
+
+                // Retrieve second topmost element on stack and AND it to register, and store result on second topmost
+                // element
+                decrementARegister(1); // @SP-2
+                printWriter.println("M=D&M");
+
+                decreaseSP();
+                break;
+
+            case "or":
+                addressTopmostStackElementAndStoreInDRegister();
+
+                // Retrieve second topmost element on stack and AND it to register, and store result on second topmost
+                // element
+                decrementARegister(1); // @SP-2
+                printWriter.println("M=D|M");
+
+                decreaseSP();
+                break;
+
+            case "not":
+                addressTopmostStackElementAndStoreInDRegister();
+
+                // Store in topmost element negative register, leave SP unaltered
+                printWriter.println("M=!D");
+                break;
+
+            default:
+                throw new IllegalArgumentException(String.format("Unrecognized arithmetic command: %s", command));
+        }
+    }
+
+    private void decrementARegister(int amount) {
+        for (int i = 0; i < amount; i++) {
+            printWriter.println("A=A-1");
+        }
+    }
+
+    private void decreaseSP() {
+        printWriter.println("@SP");
+        printWriter.println("M=M-1");
+    }
+
+    private void addressTopmostStackElementAndStoreInDRegister() {
+        printWriter.println("@SP");
+        printWriter.println("A=M");
+        decrementARegister(1); // @SP-1
+        printWriter.println("D=M");
+    }
+
+    private void generateLogicInstruction(String jmp) {
+        addressTopmostStackElementAndStoreInDRegister();
+
+        // Retrieve second topmost element on stack and subtract it from register, and store result in D
+        decrementARegister(1); // @SP-2
+        printWriter.println("D=M-D");
+
+        // If D is/is greater than/is lesser than zero, return true (-1) else return false (0)
+        printWriter.println(String.format("@LOGIC_%d", labelLogicNumber));
+        printWriter.println(String.format("D;%s", jmp));
+
+        // @SP-2
+        printWriter.println("@SP");
+        printWriter.println("A=M");
+        decrementARegister(2);
+
+        printWriter.println("M=0");
+        printWriter.println(String.format("@END_LOGIC_%d", labelLogicNumber));
+        printWriter.println("0;JMP");
+        printWriter.println(String.format("(LOGIC_%d)", labelLogicNumber));
+
+        // @SP-2
+        printWriter.println("@SP");
+        printWriter.println("A=M");
+        decrementARegister(2);
+
+        printWriter.println("M=-1");
+        printWriter.println(String.format("(END_LOGIC_%d)", labelLogicNumber));
+        decreaseSP();
+        labelLogicNumber++;
     }
 
     /**
@@ -41,7 +165,23 @@ public class CodeWriter implements Closeable {
      * @param index the index argument of the command
      */
     public void writePushPop(VmCommand command, String segment, int index) {
-        // TODO Implement
+        if (command != VmCommand.C_PUSH && !segment.equals("constant")) {
+            throw new UnsupportedOperationException("Only \"push constant N\" commands are supported");
+        }
+
+        // push constant index
+        // write constant in memory at position SP
+        printWriter.println(String.format("@%d", index));
+        printWriter.println("D=A");
+        printWriter.println("@SP");
+        printWriter.println("A=M");
+        printWriter.println("M=D");
+        increaseSP();
+    }
+
+    private void increaseSP() {
+        printWriter.println("@SP");
+        printWriter.println("M=M+1");
     }
 
     /**
